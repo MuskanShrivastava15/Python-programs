63e2fd5f0163993d4580e635d9ac4ef994b2c564
MuskanShrivastava15 <82303056+MuskanShrivastava15@users.noreply.github.com>
2021-06-20 13:05:47 +0530

    Add files via upload

---
+++ subrray with 0 sum.py
@@ -0,0 +1,14 @@
+#
+#  # sublists of a list
+# lists=[]
+# def sub_lists(l):
+#     for i in range(len(l)+1):
+#         for j in range(i):
+#             lists.append(l[j:i])
+#             print(lists)
+#
+# #drive code
+# l1=[1,2,3]
+# sub_lists(l1)
+
+
